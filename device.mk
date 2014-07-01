8c80cedb04fef748c4c854503d13a0e8b75d966a
CPA-Poke <brock.shahan@cox.net>
2014-07-01 10:22:46 -0400

    (1/2) Make ROM zip compatible with ext4/f2fs
    made tilapia specific changes
    
    Conflicts:
    	device.mk
    
    Change-Id: I65fec2110ed02bbb5e2770f10f93710a9060ef8f

--- device.mk
+++ device.mk
@@ -22,6 +22,24 @@ PRODUCT_PACKAGES := \
 PRODUCT_COPY_FILES := \
     device/asus/tilapia/fstab.grouper:root/fstab.grouper \
     device/asus/tilapia/init.tilapia.rc:root/init.grouper.rc
+    device/asus/tilapia/fstab/fstab.grouper.all-EXT4:system/extras/fstab/fstab.grouper.all-EXT4 \
+    device/asus/tilapia/fstab/fstab.grouper.all-F2FS:system/extras/fstab/fstab.grouper.all-F2FS \
+    device/asus/tilapia/fstab/fstab.grouper.data-F2FS:system/extras/fstab/fstab.grouper.data-F2FS \
+    device/asus/tilapia/tools/repack-and-flash.sh:system/extras/tools/repack-and-flash.sh \
+    device/asus/tilapia/tools/format-system.sh:system/extras/tools/format-system.sh \
+    device/asus/tilapia/tools/kernel/repackimg.sh:system/extras/tools/kernel/repackimg.sh \
+    device/asus/tilapia/tools/kernel/unpackimg.sh:system/extras/tools/kernel/unpackimg.sh \
+    device/asus/tilapia/tools/kernel/cleanup.sh:system/extras//tools/kernel/cleanup.sh \
+    device/asus/tilapia/tools/kernel/authors.txt:system/extras/tools/kernel/authors.txt \
+    device/asus/tilapia/tools/kernel/bin/aik:system/extras/tools/kernel/bin/aik \
+    device/asus/tilapia/tools/kernel/bin/busybox:system/extras/tools/kernel/bin/busybox \
+    device/asus/tilapia/tools/kernel/bin/file:system/extras/tools/kernel/bin/file \
+    device/asus/tilapia/tools/kernel/bin/lz4:system/extras/tools/kernel/bin/lz4 \
+    device/asus/tilapia/tools/kernel/bin/magic:system/extras/tools/kernel/bin/magic \
+    device/asus/tilapia/tools/kernel/bin/mkbootfs:system/extras/tools/kernel/bin/mkbootfs \
+    device/asus/tilapia/tools/kernel/bin/mkbootimg:system/extras/tools/kernel/bin/mkbootimg \
+    device/asus/tilapia/tools/kernel/bin/unpackbootimg:system/extras/tools/kernel/bin/unpackbootimg \
+    device/asus/tilapia/tools/kernel/bin/xz:system/extras/tools/kernel/bin/xz
 
 DEVICE_PACKAGE_OVERLAYS := \
     device/asus/tilapia/overlay
@@ -31,3 +49,14 @@ $(call inherit-product, device/asus/grouper/device-common.mk)
 
 # inherit from the non-open-source side, if present
 $(call inherit-product-if-exists, vendor/asus/tilapia/device-vendor.mk)
+
+# F2FS
+PRODUCT_PACKAGES += \
+    mkfs.f2fs \
+    fsck.f2fs \
+    fibmap.f2fs
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    setup_fs \
+    e2fsck
